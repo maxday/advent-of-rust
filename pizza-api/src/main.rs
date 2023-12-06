506436b95787e41c0cdc7c1ebe6b9d41b7038f86
Maxime David <maxime.david@datadoghq.com>
2023-12-05 22:24:49 -0500

    day05

--- pizza-api/src/main.rs
+++ pizza-api/src/main.rs
@@ -1,13 +1,13 @@
 mod pizza;
 
 use crate::pizza::Pizza;
-use std::env;
+use std::env::{self, VarError};
 
-fn get_port() -> String {
-    let port = env::var("PORT");
-    match port {
-        Ok(number) => number,
-        Err(e) => String::from("8000")
+fn get_port(result: Result<String, VarError>) -> i32 {
+    let default_port = 8000;
+    match result {
+        Ok(number) => number.parse::<i32>().unwrap_or(default_port),
+        Err(_) => default_port
     }
 }
 fn main() {
@@ -16,5 +16,28 @@ fn main() {
     // println!("my pizza is {:?}", pizza);
 
     
-    println!("real port is {}", get_port());
+    println!("real port is {}", get_port(std::env::var("PORT")));
+}
+
+#[cfg(test)]
+mod test {
+    use crate::get_port;
+
+    #[test]
+    fn test_valid_port() {
+        let result = Ok(String::from("1234"));
+        assert_eq!(1234, get_port(result))
+    }
+
+    #[test]
+    fn test_invalid_port() {
+        let result = Ok(String::from("1234-invalid-char"));
+        assert_eq!(8000, get_port(result))
+    }
+
+    #[test]
+    fn test_port_not_set() {
+        let result = Err(std::env::VarError::NotPresent);
+        assert_eq!(8000, get_port(result))
+    }
 }
