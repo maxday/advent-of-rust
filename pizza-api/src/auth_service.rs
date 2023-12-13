6c220d6714ea434ef8aeadee0c5515c96b4cf7ca
Maxime David <maxime.david@datadoghq.com>
2023-12-12 23:09:59 -0500

    day11

--- pizza-api/src/auth_service.rs
+++ pizza-api/src/auth_service.rs
@@ -1,4 +1,6 @@
-use actix_web::{HttpResponse, get, http::header::LOCATION};
+use actix_web::{HttpResponse, get, http::header::LOCATION, HttpRequest, web};
+use reqwest::{Client, header::ACCEPT};
+use serde::Deserialize;
 
 #[get("/sign_in")]
 async fn get_sign_in() -> HttpResponse {
@@ -13,7 +15,39 @@ async fn get_sign_in() -> HttpResponse {
     }
 }
 
+#[derive(Deserialize)]
+struct CodeRequest {
+    code: String
+}
+
 #[get("/callback")]
-async fn get_callback() -> HttpResponse {
+async fn get_callback(req: web::Query<CodeRequest>) -> HttpResponse {
+    exchange_code_for_token(&req.code).await;
     HttpResponse::Ok().body("this is /callback")
+}
+
+async fn exchange_code_for_token(code: &str) -> String {
+    // this is bad
+    let url = std::env::var("GITHUB_TOKEN_URL").unwrap();
+    let client_id = std::env::var("GITHUB_CLIENT_ID").unwrap();
+    let client_secret_id = std::env::var("GITHUB_CLIENT_SECRET_ID").unwrap();
+    let payload = [
+        ("client_id", client_id),
+        ("client_secret", client_secret_id),
+        ("code", String::from(code))
+    ];
+
+    let client = Client::new();
+    // this is bad, we need to refactor tomorrow!
+    let response = client
+    .post(url)
+    .form(&payload)
+    .header(ACCEPT, "application/json")
+    .send()
+    .await
+    .unwrap();
+
+    println!("response = {:?}", response.text().await);
+
+    return String::from("my-token");
 }
